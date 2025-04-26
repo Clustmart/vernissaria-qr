9396c16a140ad1168954be5125ae894a48ff22ab
Paul Wasicsek <paul.wasicsek@gmail.com>
2025-04-26 18:20:41 +0300

    Bug: fixed dashboard widget visits count

--- vernissaria-qr/includes/qr-settings.php
+++ vernissaria-qr/includes/qr-settings.php
@@ -354,7 +354,7 @@ function vernissaria_dashboard_widget() {
 }
 
 /**
- * Visitor stats dashboard widget for last 30 days 
+ * Visitor stats dashboard widget for last 30 days
  */
 function vernissaria_visitor_stats_widget() {
     // Enqueue Chart.js
@@ -362,8 +362,10 @@ function vernissaria_visitor_stats_widget() {
 
     // Get API URL from settings
     $api_url = get_option('vernissaria_api_url', 'https://vernissaria.qraft.link');
+    $site_url = get_site_url();
     $domain = parse_url($site_url, PHP_URL_HOST);
-    $endpoint = $api_url . '/api/stats/daily?days=30&domain=';    
+    $endpoint = $api_url . '/api/stats/daily?days=30&domain='. urlencode($domain);
+    
     
     // Make API request
     $response = wp_remote_get($endpoint);
